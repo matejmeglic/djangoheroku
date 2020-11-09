bf23abd7ff403b194297463ffd89f1e8d958a2a1
matejmeglic <matej@matejmeglic.com>
2020-11-10 00:00:47 +0100

    5

--- djangoheroku/settings.py
+++ djangoheroku/settings.py
@@ -11,7 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-
+import django_heroku
 from dotenv import load_dotenv
 
 load_dotenv()
@@ -125,3 +125,5 @@ USE_TZ = True
 STATIC_URL = "/static/"
 
 STATIC_ROOT = os.path.join(BASE_DIR, "staticfiles")
+
+django_heroku.settings(locals())
